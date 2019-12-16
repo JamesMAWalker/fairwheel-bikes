b99a16f6c32dce2082be64fa08cd32c9df017e6b
James Walker <jamesmawalker@gmail.com>
2019-12-16 15:00:17 +0700

    Add signin and button components.

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./signin-signup.styles.scss";
+import Signin from '../../components/signin/signin.component';
+
+const SigninSignup = () => (
+    <div className="signin-signup">
+        <Signin />
+    </div>
+);
+
+export default SigninSignup;
