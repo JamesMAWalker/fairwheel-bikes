8f7b00c834810fb1c81fc1ca9b5f71602835daa1
James Walker <jamesmawalker@gmail.com>
2019-12-28 13:18:06 +0700

    Finish signin component handleSubmit.

--- src/components/signup/signup.component.jsx
+++ src/components/signup/signup.component.jsx
@@ -56,47 +56,47 @@ class SignUp extends React.Component {
     
     render() {
         const { displayName, email, password, confirmPassword } = this.state;
-        return(
-            <div className="signup">
-                <h2 className="title">I do not have an account</h2>
-                <span>Sign up with your email and password</span>
-                <form className="signup-form" onSubmit={this.handleSubmit}>
-                    <FormInput
-                        type='text'
-                        name='displayName'
-                        value={displayName}
-                        onChange={this.handleChange}
-                        label='Display Name'
-                        required
-                    />
-                    <FormInput
-                        type='email'
-                        name='email'
-                        value={email}
-                        onChange={this.handleChange}
-                        label='Email'
-                        required
-                    />
-                    <FormInput
-                        type='text'
-                        name='password'
-                        value={password}
-                        onChange={this.handleChange}
-                        label='Password'
-                        required
-                    />
-                    <FormInput
-                        type='text'
-                        name='confirmPassword'
-                        value={confirmPassword}
-                        onChange={this.handleChange}
-                        label='Confirm Password'
-                        required
-                    />
-                    <CustomButton type='submit'>Sign Up</CustomButton>
-                </form>
-            </div>
-        )
+        return (
+          <div className="signup">
+            <h2 className="title">I do not have an account</h2>
+            <span>Sign up with your email and password</span>
+            <form className="signup-form" onSubmit={this.handleSubmit}>
+              <FormInput
+                type="text"
+                name="displayName"
+                value={this.state.displayName}
+                onChange={this.handleChange}
+                label="Display Name"
+                required
+              />
+              <FormInput
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                label="Email"
+                required
+              />
+              <FormInput
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                label="Password"
+                required
+              />
+              <FormInput
+                type="password"
+                name="confirmPassword"
+                value={this.state.confirmPassword}
+                onChange={this.handleChange}
+                label="Confirm Password"
+                required
+              />
+              <CustomButton type="submit">Sign Up</CustomButton>
+            </form>
+          </div>
+        );
     }
 }
 
