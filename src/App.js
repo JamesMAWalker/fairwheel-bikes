599f11dbdc2c5d148898e052e94d3f57c75e6ed6
James Walker <jamesmawalker@gmail.com>
2019-12-31 16:29:17 +0700

    Add user types file, importing where necessary.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
 
 import './App.css';
@@ -58,7 +57,14 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SigninSignup} />
+          <Route exact path="/signin" render={() => 
+            this.props.currentUser ? (
+              <Redirect to='/'/> 
+              ) : (
+                <SigninSignup />
+              )
+            } 
+          />
         </Switch>
       </div>
     );
@@ -66,8 +72,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
