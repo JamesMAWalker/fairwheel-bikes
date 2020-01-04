5f511bd7ef2a172805057e4102404b221a60ff40
James Walker <jamesmawalker@gmail.com>
2020-01-04 12:34:45 +0700

    Refactor MStP with structured selectors.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -10,18 +11,10 @@ import Header from './components/header/header.component';
 import SigninSignup from './pages/signin-signup/signin-signup.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 
 class App extends React.Component {
-
-  // constructor() {
-    
-  //   super();
-
-  //   this.state = {
-  //     currentUser: null
-  //   }
-  // }
 
   unsubscribeFromAuth = null;
 
@@ -72,8 +65,8 @@ class App extends React.Component {
   
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
